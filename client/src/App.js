227d1a883e51945bd4eeda551fb0e3ed0300f5da
Olivia Owen <olivia.owen@hotmail.com>
2021-05-24 21:07:59 +0100

    Updated bootstrap link

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Home from "./pages";
-import ProfileSettings from "./pages/ProfileSettings.js";
+// import ProfileSettings from "./pages/ProfileSettings.js";
+import SignUp from "./pages/SignUp.js";
 // import NoMatch from "./pages/NoMatch";
 // import Navbar from "./components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -12,7 +14,8 @@ function App() {
         {/* <Navbar /> */}
         <Switch>
           {/* <Route exact path="/" component={Home} /> */}
-          <Route exact path="/profile-settings" component={ProfileSettings} />
+          {/* <Route exact path="/profile-settings" component={ProfileSettings} /> */}
+          <Route exact path="/signup" component={SignUp} />
         </Switch>
       </div>
     </Router>
