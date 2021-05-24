3f5fa61f472ce9b6de889ed0d339d38296afa518
kate <katy.madden99@gmail.com>
2021-05-24 19:26:54 +0100

    created profilesetting page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import "../src/pages";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <ProfileSettings></ProfileSettings>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ client/src/pages/ProfileSettings.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Container, Row, Col} from "../components/Grid";
+
+function ProfSetting() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-12">
+        <p>test</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default ProfSetting;
