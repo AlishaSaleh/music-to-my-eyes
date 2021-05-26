302a35f6239c129cbfbd201d267925a666047f47
kate <katy.madden99@gmail.com>
2021-05-26 08:35:08 +0100

    added form to profile settings page

--- client/src/components/UserSettingsForm/index.js
+++ client/src/components/UserSettingsForm/index.js
@@ -1,37 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
 
-function UserSettingsForm(props) {
-  return 
-//   <Form>
-//   <Form.Group controlId="exampleForm.ControlInput1">
-//     <Form.Label>Email address</Form.Label>
-//     <Form.Control type="email" placeholder="name@example.com" />
-//   </Form.Group>
-//   <Form.Group controlId="exampleForm.ControlSelect1">
-//     <Form.Label>Example select</Form.Label>
-//     <Form.Control as="select">
-//       <option>1</option>
-//       <option>2</option>
-//       <option>3</option>
-//       <option>4</option>
-//       <option>5</option>
-//     </Form.Control>
-//   </Form.Group>
-//   <Form.Group controlId="exampleForm.ControlSelect2">
-//     <Form.Label>Example multiple select</Form.Label>
-//     <Form.Control as="select" multiple>
-//       <option>1</option>
-//       <option>2</option>
-//       <option>3</option>
-//       <option>4</option>
-//       <option>5</option>
-//     </Form.Control>
-//   </Form.Group>
-//   <Form.Group controlId="exampleForm.ControlTextarea1">
-//     <Form.Label>Example textarea</Form.Label>
-//     <Form.Control as="textarea" rows={3} />
-//   </Form.Group>
-// </Form>;
+
+function UserSettingsForm() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form.Row>
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>First name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="First name"
+            defaultValue=""
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Last name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Last name"
+            defaultValue=""
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Date of Birth</Form.Label>
+          <Form.Control placeholder="Date of Birth" disabled />
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} md="6" controlId="validationCustom03">
+          <Form.Label>City</Form.Label>
+          <Form.Control type="text" placeholder="City" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="3" controlId="validationCustom04">
+          <Form.Label>Country</Form.Label>
+          <Form.Control type="text" placeholder="Country" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid country.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Form.Row>
+      <Button type="submit">Submit form</Button>
+    </Form>
+  );
 }
 
-export default ProfilePic;
+export default UserSettingsForm;
