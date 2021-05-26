e735d6a5b9940a0824c2bfb910a66f60942de0b7
Karen <karenyaa@live.co.uk>
2021-05-26 21:28:19 +0100

    added approuter component to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import AppRouter from './router/AppRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
@@ -9,6 +12,9 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>,
   document.getElementById('root')
 );
 
