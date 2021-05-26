c91cebf0fe8d7a8ab4e1d77f4f25ab6f023f04a8
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-05-26 14:40:46 +0100

    User routes added

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,1 +1,13 @@
 // user sign up, login and get routes
+const express = require('express');
+const router = express.Router();
+const userController = require("../../controllers/userController");
+// will need to require whichever middleware we use for authenticatiton here
+
+// user/[route]
+router.get('/', userController.getUser);
+router.post('/signup', userController.signUp);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+
+module.exports = router;
