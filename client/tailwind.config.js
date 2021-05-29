75491aed3ab16de144c87b484dbeb2ce99df7423
Zarin Salim <zarin_96@hotmail.co.uk>
2021-05-29 10:45:42 +0100

    created homepage, updated navbar component, updated tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+        'mint': '#A3BFBF',
+        'mint-dark': '#356161',
+        'mint-light': '#7EE6E6'
+    },
     extend: {},
   },
   variants: {
