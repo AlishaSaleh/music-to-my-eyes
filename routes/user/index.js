255ac77f6898439a59c02a433e8546e12c188402
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-06-01 10:37:39 +0100

    User route working in postman

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,13 +1,106 @@
 // user sign up, login and get routes
 const express = require('express');
 const router = express.Router();
-const userController = require("../../controllers/userController");
+//const userController = require("../../controllers/userController");
 // will need to require whichever middleware we use for authenticatiton here
 
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const User = require("../../models/User");
+
+// input validation
+const validateRegisterInput = require("../../validation/signup");
+const validateLoginInput = require("../../validation/login");
+
 // user/[route]
-router.get('/', userController.getUser);
-router.post('/signup', userController.signUp);
-router.post('/login', userController.login);
-router.post('/logout', userController.logout);
+//router.get('/', userController.getUser);
+// 1: signup using the userController
+// router.post('/signup', userController.signUp);
+// 2: OR signup using function
+router.post("/signup", (req, res) => {
+    // Form validation
+    const { errors, isValid } = validateRegisterInput(req.body);
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    } User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            return res.status(400).json({ email: "Email already exists" });
+        } else {
+            // newUser not currently read with the hashing function removed
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+            // Hashing already done in model but this is alternative place for it if it doesn't work
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+});
+// 1: login in with userController
+// router.post('/login', userController.login);
+// 2: OR login with  function
+router.post("/login", (req, res) => {
+    // Form validation
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Find user by email
+    User.findOne({ email }).then(user => {
+        // Check if user exists
+        if (!user) {
+            return res.status(404).json({ emailnotfound: "Email not found" });
+        }
+        // Check password
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                // User matched
+                // Create JWT Payload
+                const payload = {
+                    id: user.id,
+                    name: user.name
+                };
+                // Sign token
+                jwt.sign(
+                    payload,
+                    keys.secretOrKey,
+                    {
+                        expiresIn: 31556926 // 1 year in seconds
+                    },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
+                        });
+                    }
+                );
+            } else {
+                return res
+                    .status(400)
+                    .json({ passwordincorrect: "Password incorrect" });
+            }
+        });
+    });
+});
+
+//router.post('/logout', userController.logout);
 
 module.exports = router;
