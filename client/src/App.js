73fbbf8d1a55fac59603831f9585cb781603ceb5
Zarin Salim <zarin_96@hotmail.co.uk>
2021-06-01 20:33:00 +0100

    created simple dashboard to work from

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import ProfileSettings from "./pages/ProfileSettings.js";
 // import NoMatch from "./pages/NoMatch";
 import Navbar from "./components/Navbar";
 import error404 from "./pages/404";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/profile-settings" component={ProfileSettings} />
           <Route exact path="/error404" component={error404} />
+          <Route exact path="/profile" component={Profile} />
         </Switch>
       </div>
     </Router>
