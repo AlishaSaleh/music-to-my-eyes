ed45f5ae1b29c3013bb564583d3ff018696a4642
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-06-03 10:06:07 +0100

    Setting up redux reducers

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,17 +3,17 @@ import thunk from 'redux-thunk';
 import albumsReducer from '../reducers/albums';
 import artistsReducer from '../reducers/artists';
 import playlistReducer from '../reducers/playlist';
-
-const initialState = {};
-const middleware = [thunk];
-
+import authReducer from '../reducers/authReducers';
+import errorReducer from '../reducers/errorReducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   combineReducers({
     albums: albumsReducer,
     artists: artistsReducer,
-    playlist: playlistReducer
+    playlist: playlistReducer,
+    auth: authReducer,
+    errors: errorReducer
   }),
   composeEnhancers(applyMiddleware(thunk))
 );
