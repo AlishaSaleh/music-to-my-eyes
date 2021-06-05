5aa6a0c1810614091c9c0a497706ddab6cb9f3a5
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-06-05 11:03:41 +0100

    cross-branch file merging

--- client/src/pages/ProfileSettings.js
+++ client/src/pages/ProfileSettings.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {Container, Row, Col} from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
-import ProfilePic from "../components/ProfilePic";
-import UserSettingsForm from "../components/UserSettingsForm";
+import Divider from "../components/Divider";
+import {UserSettingsForm} from "../components/UserSettingsForm";
+
 
 function ProfileSettings() {
   return (
@@ -11,13 +12,12 @@ function ProfileSettings() {
         <Col size="md-12">
           <Jumbotron>
             <h1>Profile Settings</h1>
+            <Divider />
           </Jumbotron>
-          <ProfilePic src="https://randomuser.me/api/portraits/men/47.jpg" />
-          <UserSettingsForm />
+          <UserSettingsForm/>
         </Col>
       </Row>
     </Container>
   );
 }
-
 export default ProfileSettings;
