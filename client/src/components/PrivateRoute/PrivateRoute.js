bb785e644cba4bdd38076922ee96a853f3b4d630
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-06-07 18:28:14 +0100

    Auth into dashboard working!

--- client/src/components/PrivateRoute/PrivateRoute.js
+++ client/src/components/PrivateRoute/PrivateRoute.js
@@ -5,34 +5,11 @@ import { isAuth } from '../../utils/isAuth';
 // Check if the user is logged in
 export function PrivateRoute({ component: Component, ...rest }) {
 
-    const [isAuthed, setIsAuthed] = useState(false);
-    
-    // console.log(isAuth())
-    // if (isAuth() === true) {
-    //     setIsAuthed(true)
-    // }
-    // console.log(isAuthed);
-
-    useEffect(() => {
-        console.log(isAuth())
-        // ^ returning true
-
-        // if (isAuth() === true) {
-        //     return setIsAuthed()
-        // }
-       
-        setIsAuthed(isAuth());
-        // returning false when should be true:
-        console.log(isAuthed);
-    }, []);
-    // this one not returning at all
-    //console.log(isAuthed);
-
     return (
         <Route
             {...rest}
             render={props =>
-                isAuthed
+                isAuth()
                     ? <Component {...props} />
                     : <Redirect
                         to='/login'
