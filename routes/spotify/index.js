632e1b7398de6a3d653fdece94e4c95a5ff41bbd
Karen <karenyaa@live.co.uk>
2021-06-07 19:39:30 +0100

    added post route to login

--- routes/spotify/index.js
+++ routes/spotify/index.js
@@ -23,6 +23,28 @@ router.post("/refresh", (req, res) => {
         console.log(err)
         res.sendStatus(400)
       })
+  });
+
+  router.post("/login", (req, res) => {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: process.env.REACT_APP_AUTHORIZE_URL,
+        clientId: process.env.REACT_APP_CLIENT_ID,
+        clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+    })
+  
+    spotifyApi
+      .authorizationCodeGrant(code)
+      .then(data => {
+        res.json({
+          accessToken: data.body.access_token,
+          refreshToken: data.body.refresh_token,
+          expiresIn: data.body.expires_in,
+        })
+      })
+      .catch(err => {
+        res.sendStatus(400)
+      })
   })
 
 module.exports = router; 
