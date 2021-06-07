c514e5f0df23746b5e6d8a4b23ecf90e5e91b5f1
Karen <karenyaa@live.co.uk>
2021-06-07 20:31:27 +0100

    adding useeffect for expires/refresh tokens

--- client/src/components/SpotifyComp/spotifyUserAuth.js
+++ client/src/components/SpotifyComp/spotifyUserAuth.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 
-export default function useAuth(code) {
+export default function spotifyUserAuth(code) {
     useEffect(() => {
         axios
           .post("http://localhost:3001/spotify/login", {
@@ -17,5 +17,25 @@ export default function useAuth(code) {
             window.location = "/spotify"
           })
       }, [code])
+
+      useEffect(() => {
+        if (!refreshToken || !expiresIn) return
+        const interval = setInterval(() => {
+          axios
+            .post("http://localhost:3001/refresh", {
+              refreshToken,
+            })
+            .then(res => {
+              setAccessToken(res.data.accessToken)
+              setExpiresIn(res.data.expiresIn)
+            })
+            .catch(() => {
+              window.location = "/spotify"
+            })
+        }, (expiresIn - 60) * 1000)
     
+        return () => clearInterval(interval)
+      }, [refreshToken, expiresIn])
+    
+      return accessToken
 }
