a92fed8d5c1fc8497014d7be5db0392c2e01b800
Karen <karenyaa@live.co.uk>
2021-06-07 23:17:33 +0100

    changing state names and returning track information

--- client/src/components/SpotifyComp/SpotifyDashboard.js
+++ client/src/components/SpotifyComp/SpotifyDashboard.js
@@ -3,6 +3,7 @@ import { Container, Form } from 'react-bootstrap'
 import useAuth from "./spotifyUserAuth"
 import SpotifyWebApi from 'spotify-web-api-node'
 
+
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.REACT_APP_CLIENT_ID,
 })
@@ -15,46 +16,48 @@ export default function SpotifyDashboard({ code }) {
     useEffect(() => {
         if (!accessToken) return
         spotifyApi.setAccessToken(accessToken)
-      }, [accessToken])
-    
-      useEffect(() => {
+    }, [accessToken])
+
+    useEffect(() => {
         if (!accessToken) return
         spotifyApi.setAccessToken(accessToken)
-      }, [accessToken])
-    
-      useEffect(() => {
-        if (!search) return setSearchResults([])
+    }, [accessToken])
+
+    useEffect(() => {
+        if (!songSearch) return setSearchResults([])
         if (!accessToken) return
-    
+
         let cancel = false
-        spotifyApi.searchTracks(search).then(res => {
-          if (cancel) return
-          setSearchResults(
-            res.body.tracks.items.map(track => {
-              const smallestAlbumImage = track.album.images.reduce(
-                (smallest, image) => {
-                  if (image.height < smallest.height) return image
-                  return smallest
-                },
-                track.album.images[0]
-              )
-    
-              return {
-                artist: track.artists[0].name,
-                title: track.name,
-                uri: track.uri,
-                albumUrl: smallestAlbumImage.url,
-              }
-            })
-          )
-        })
+        spotifyApi.searchTracks(songSearch).then(res => {
+            if (cancel) return
+            setSearchResults(
+                res.body.tracks.items.map(track => {
+                    const smallestAlbumImage = track.album.images.reduce(
+                        (smallest, image) => {
+                            if (image.height < smallest.height) return image
+                            return smallest
+                        },
+                        track.album.images[0]
+                    )
+
+                    return {
+                        artist: track.artists[0].name,
+                        title: track.name,
+                        uri: track.uri,
+                        albumUrl: smallestAlbumImage.url,
+                    }
+                })
+            )
+        });
+        return () => (cancel = true)
+    }, [songSearch, accessToken])
     return (
         <Container>
-            <Form.Control 
-            type="search" 
-            placeholder="Search Songs or Artists" 
-            value={songSearch} 
-            onChange={e => setSongSearch(e.target.value)} 
+            <Form.Control
+                type="search"
+                placeholder="Search Songs or Artists"
+                value={songSearch}
+                onChange={e => setSongSearch(e.target.value)}
             />
             <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }} >
                 Songs
