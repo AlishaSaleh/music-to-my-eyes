ad75a4a8eac44055cbedc537ad3e09a8f57d1361
Karen <karenyaa@live.co.uk>
2021-06-07 13:04:13 +0100

    added basic styling for button in spotify login page

--- client/src/components/SpotifyComp/SpotifyLogin.js
+++ client/src/components/SpotifyComp/SpotifyLogin.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import Container from 'react-bootstrap'
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
-const AUTH_URL =`${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
-
-export default function SpotifyLogin () {
+export default function SpotifyLogin() {
     return (
         <div>
-
+            <Container
+                className="d-flex justify-content-center align-items-center"
+                style={{ minHeight: "100vh" }}
+            >
+                <a className="btn btn-success btn-lg" href={AUTH_URL}>
+                    Login With Spotify
+      </a>
+            </Container>
         </div>
     )
 }
