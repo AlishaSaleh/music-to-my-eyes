a204c8a4fa4316e21a37f2d72f173cca5dfc6b87
Karen <karenyaa@live.co.uk>
2021-06-07 13:00:23 +0100

    added post route to refresh spotify token

--- routes/spotify/index.js
+++ routes/spotify/index.js
@@ -2,4 +2,27 @@ const express = require('express');
 const router = express.Router();
 const SpotifyWebApi = require("spotify-web-api-node")
 
+router.post("/refresh", (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+      redirectUri: process.env.REACT_APP_AUTHORIZE_URL,
+      clientId: process.env.REACT_APP_CLIENT_ID,
+      clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+      refreshToken,
+    })
+  
+    spotifyApi
+      .refreshAccessToken()
+      .then(data => {
+        res.json({
+          accessToken: data.body.accessToken,
+          expiresIn: data.body.expiresIn,
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        res.sendStatus(400)
+      })
+  })
+
 module.exports = router; 
