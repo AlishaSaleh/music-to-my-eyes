34ddc98be2a21fd13b217176f4a7ca7fd89a927e
Karen <karenyaa@live.co.uk>
2021-06-07 12:53:25 +0100

    building auth url to log into spotify

---
+++ client/src/components/SpotifyComp/SpotifyLogin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AUTH_URL =`${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+
+export default function SpotifyLogin () {
+    return (
+        <div>
+
+        </div>
+    )
+}
