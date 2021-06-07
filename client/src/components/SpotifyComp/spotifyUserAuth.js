31df5d7046c839f98298cf8f7775bebd3fe48e7e
Karen <karenyaa@live.co.uk>
2021-06-07 20:13:30 +0100

    testing spotify user auth

--- client/src/components/SpotifyComp/spotifyUserAuth.js
+++ client/src/components/SpotifyComp/spotifyUserAuth.js
@@ -1,4 +1,21 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 
-export default function useAuth(code) {}
+export default function useAuth(code) {
+    useEffect(() => {
+        axios
+          .post("http://localhost:3001/spotify/login", {
+            code,
+          })
+          .then(res => {
+            setAccessToken(res.data.accessToken)
+            setRefreshToken(res.data.refreshToken)
+            setExpiresIn(res.data.expiresIn)
+            window.history.pushState({}, null, "/spotify")
+          })
+          .catch(() => {
+            window.location = "/spotify"
+          })
+      }, [code])
+    
+}
--- client/src/pages/SpotifyLogin.js
+++ client/src/pages/SpotifyLogin.js
@@ -6,7 +6,8 @@ const {
     REACT_APP_REDIRECT_URL
   } = process.env;
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+console.log(REACT_APP_CLIENT_ID)
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${REACT_APP_CLIENT_ID}&&response_type=code&redirect_uri=${REACT_APP_REDIRECT_URL}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
 export default function SpotifyLogin() {
     return (
