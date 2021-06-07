6b0078604701ec87a8827158e3b3b97b24febc3b
Olivia Owen <olivia.owen@hotmail.com>
2021-06-07 20:51:34 +0100

    Created age validation function

---
+++ validation/ageValidation.js
@@ -0,0 +1,17 @@
+
+module.exports = function ageValidate()  {
+    var dob = new Date(dob);
+    //calculate month difference from current date in time
+    var month_diff = Date.now() - dob.getTime();
+
+    //convert the calculated difference in date format
+    var age_dt = new Date(month_diff);
+
+    //extract year from date    
+    var year = age_dt.getUTCFullYear();
+
+    //now calculate the age of the user
+    var age = Math.abs(year - 1970);
+
+
+}
