cd931fd546dde3c839d0af3ba7b8f178bb8cd04e
Karen <karenyaa@live.co.uk>
2021-06-07 23:15:41 +0100

    testing useEffect to return track information

--- client/src/components/SpotifyComp/SpotifyDashboard.js
+++ client/src/components/SpotifyComp/SpotifyDashboard.js
@@ -18,10 +18,36 @@ export default function SpotifyDashboard({ code }) {
       }, [accessToken])
     
       useEffect(() => {
+        if (!accessToken) return
+        spotifyApi.setAccessToken(accessToken)
+      }, [accessToken])
+    
+      useEffect(() => {
         if (!search) return setSearchResults([])
         if (!accessToken) return
-
-      });
+    
+        let cancel = false
+        spotifyApi.searchTracks(search).then(res => {
+          if (cancel) return
+          setSearchResults(
+            res.body.tracks.items.map(track => {
+              const smallestAlbumImage = track.album.images.reduce(
+                (smallest, image) => {
+                  if (image.height < smallest.height) return image
+                  return smallest
+                },
+                track.album.images[0]
+              )
+    
+              return {
+                artist: track.artists[0].name,
+                title: track.name,
+                uri: track.uri,
+                albumUrl: smallestAlbumImage.url,
+              }
+            })
+          )
+        })
     return (
         <Container>
             <Form.Control 
