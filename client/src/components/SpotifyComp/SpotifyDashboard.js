d604bdbb3623826e9d058fba64fbbc81f7efcc2f
Karen <karenyaa@live.co.uk>
2021-06-07 22:11:56 +0100

    setting states to handle song searches

--- client/src/components/SpotifyComp/SpotifyDashboard.js
+++ client/src/components/SpotifyComp/SpotifyDashboard.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import {Container, Form} from 'react-bootstrap'
+import { useState, useEffect } from 'react'
+import { Container, Form } from 'react-bootstrap'
 import spotifyUserAuth from "./spotifyUserAuth"
 
-export default function SpotifyDashboard ({code}) {
-    const accessToken = spotifyUserAuth(code)
+export default function SpotifyDashboard({ code }) {
+    const accessToken = spotifyUserAuth(code);
+    const [songSearch, setSongSearch] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
     return (
         <Container>
-            <Form.Control type="search" placeholder="Search Songs or Artists" value={search} onChange={e => setSearch(e.target.value)} />
+            <Form.Control 
+            type="search" 
+            placeholder="Search Songs or Artists" 
+            value={songSearch} 
+            onChange={e => setSongSearch(e.target.value)} 
+            />
+            <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }} >
+                Songs
+            </div>
         </Container>
     )
 }
