6fd4a3e1796b805e971d4b9d2d2af40c3b27b618
Karen <karenyaa@live.co.uk>
2021-06-07 23:27:40 +0100

    finished basic styling for card displaying track results

--- client/src/components/SpotifyComp/TrackSearchResult.js
+++ client/src/components/SpotifyComp/TrackSearchResult.js
@@ -1,7 +1,17 @@
 import React from "react"
 
 export default function TrackSearchResult({ track, chooseTrack }) {
+    chooseTrack(track)
     return (
-        <div></div>
+        <div
+        className="d-flex m-2 align-items-center"
+        // style={{ cursor: "pointer" }}
+      >
+        <img src={track.albumUrl} style={{ height: "64px", width: "64px" }} />
+        <div className="ml-3">
+          <div>{track.title}</div>
+          <div className="text-muted">{track.artist}</div>
+        </div>
+      </div>
     )
 }
