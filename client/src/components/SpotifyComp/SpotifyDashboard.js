8083cdb0103653db906a5218e9f908a0de3011bd
Karen <karenyaa@live.co.uk>
2021-06-07 22:02:43 +0100

    creating dashboard page to render spotify search results

--- client/src/components/SpotifyComp/SpotifyDashboard.js
+++ client/src/components/SpotifyComp/SpotifyDashboard.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import {Container, Form} from 'react-bootstrap'
+import spotifyUserAuth from "./spotifyUserAuth"
 
 export default function SpotifyDashboard ({code}) {
+    const accessToken = spotifyUserAuth(code)
     return (
         <div>{code}</div>
     )
