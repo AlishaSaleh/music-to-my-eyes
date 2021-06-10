165638389a15728c4bebe5a409a057bd7554ab71
Zarin Salim <zarin_96@hotmail.co.uk>
2021-06-10 03:41:16 +0100

    edited user dashboard, added spotify button, fixed responsiveness

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { isAuth } from '../utils/isAuth';
+import HeroBg from "../components/HeroBg"
 
 function Profile() {
 
@@ -7,39 +8,20 @@ function Profile() {
 
         <div>
             <main className="profile-page">
+
+                {/* bg header */}
                 <section className="relative block" style={{ height: "500px" }}>
                     <div
-                        className="absolute top-0 w-full h-full bg-center bg-cover"
-                        style={{
-                            backgroundImage:
-                                "url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2710&q=80')"
-                        }}
-                    >
+                        className="absolute top-0 w-full h-full bg-center bg-cover">
+                            <HeroBg />
                         <span
                             id="blackOverlay"
-                            className="w-full h-full absolute opacity-50 bg-black"
+                            className="w-full h-full absolute opacity-30 bg-black"
                         ></span>
                     </div>
-                    <div
-                        className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
-                        style={{ height: "70px" }}
-                    >
-                        <svg
-                            className="absolute bottom-0 overflow-hidden"
-                            xmlns="http://www.w3.org/2000/svg"
-                            preserveAspectRatio="none"
-                            version="1.1"
-                            viewBox="0 0 2560 100"
-                            x="0"
-                            y="0"
-                        >
-                            <polygon
-                                className="text-gray-300 fill-current"
-                                points="2560 0 2560 100 0 100"
-                            ></polygon>
-                        </svg>
-                    </div>
                 </section>
+
+                {/* profile */}
                 <section className="relative py-16 bg-gray-300">
                     <div className="container mx-auto px-4">
                         <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
@@ -50,7 +32,7 @@ function Profile() {
                                             {/* profile picture */}
                                             {/* <img
                         alt="..."
-                        src={require("assets/img/team-2-800x800.jpg").default}
+                        src={require(User.image).default}
                         className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
                         style={{ maxWidth: "150px" }}
                       /> */}
@@ -60,28 +42,32 @@ function Profile() {
                                 <div className="text-center mt-12">
                                     <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
                                         NAME
-                  </h3>
+                                    </h3>
                                     <button
                                         className="bg-gradient-to-r from-pink to-purple uppercase text-white font-bold hover:sh adow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
                                         type="button"
-                                        style={{ transition: "all .15s ease" }}
-                                    >
+                                        style={{ transition: "all .15s ease" }}>
                                         Settings
-                      </button>
+                                     </button>
                                 </div>
 
-                    <div>
-                        <div className="flex flex-wrap -mx-3 overflow-hidden">
-                            <div className="my-3 px-3 w-1/2 overflow-hidden bg-red rounded-l bg-opacity-50">
-                                <h1>My Music</h1>
-                                <p> User's chosen music will go here</p>
-                            </div>
-                            <div className="my-3 px-3 w-1/2 overflow-hidden bg-red rounded-l bg-opacity-50">
-                                <h1>Messenger</h1>
-                                <p> Matches will show here and then you can message them, will lead to chat</p>
-                            </div>
-                        </div>
-                    </div>
+                                <div className="relative grid grid-cols-1 md:grid-cols-2 gap-2">
+
+                                    {/* music */}
+                                    <div className="col-span-1 text-center p-5 pt-20 bg-gradient-to-r from-red to-pink rounded-2xl">
+                                        <h1 className="text-2xl pb-6">My Music</h1>
+                                        <button type="button" className="transition duration-300 btn-shadow ease-in-out transform hover:scale-110 mx-3 py-2 px-4 rounded border p-4">Add your favourite songs
+                                        <img className="pt-2" alt="spotify logo" src="https://drive.google.com/thumbnail?id=1bj86C-TKkcSqVzxLqdoHLqxuDU68OdPm" /></button>
+                                    </div>
+
+                                    {/* matches */}
+                                    <div className="col-span-1 text-center p-5 pt-20 bg-gradient-to-l from-red to-pink rounded-2xl">
+                                        <h1 className="text-2xl">My Matches</h1>
+                                        <p>It's time to find your playlist partner! </p>
+                                    </div>
+
+                                </div>
+
                             </div>
                         </div>
                     </div>
