be0a08dc6558b48f1a65dbe7ecbb9434528abb9d
Karen <karenyaa@live.co.uk>
2021-06-11 20:17:58 +0100

    removed useEffect from Match page

--- client/src/pages/Match.js
+++ client/src/pages/Match.js
@@ -5,26 +5,26 @@ import API from "../utils/API";
 
 function Match() {
 
-    const [loggedUser, setLogged] = useState()
+    // const [loggedUser, setLogged] = useState()
 
-    useEffect(() => {
-        async function fetchData() {
-            API.getMatch().then(response => {
-                console.log(response.data)
-                setLogged(response.data.user);
-            });
-        };
-        fetchData()
-    }, []);
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         API.getMatch().then(response => {
+    //             console.log(response.data)
+    //             setLogged(response.data.user);
+    //         });
+    //     };
+    //     fetchData()
+    // }, []);
 
-    console.log(loggedUser)
+    //console.log(loggedUser)
 
 
     return (
         <div>
             <div className="bg-image w-full h-auto flex flex-wrap justify-center items-center py-5">
                 <div className="backdrop h-screen bg-white bg-opacity-10 rounded p-3 text-black border border-gray-300 shadow-lg">
-                    <Card userId={loggedUser}/>
+                    <Card />
                 </div>
             </div>
         </div>
