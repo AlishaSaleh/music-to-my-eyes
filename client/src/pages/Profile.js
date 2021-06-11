846c6db8130a51f00af8a94a65f2634f18daef8f
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-06-11 15:25:41 +0100

    Trying to bring back name of matches

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,20 +3,45 @@ import API from "../utils/API";
 import HeroBg from "../components/HeroBg"
 
 function Profile() {
-  
-    const [loggedUser, setLogged] = useState({})
+
+    const [loggedName, setName] = useState();
+    const [loggedMatches, setMatches] = useState([]);
+    const [loggedImage, setImage] = useState([]);
+
 
     useEffect(() => {
         async function fetchData() {
             API.getDash().then(response => {
                 console.log(response.data)
-                setLogged(response.data);
+                setName(response.data.name);
+                setMatches(response.data.matches);
+                setImage(response.data.image);
+
             });
         };
         fetchData()
     }, []);
 
-    console.log(loggedUser)
+    const matchArr = [];
+
+    useEffect(() => {
+        async function fetchMatch() {
+            loggedMatches.map(matchId => API.getUser(matchId).then(response => {
+             console.log(response.data.name);
+             matchArr.push(response.data.name);
+             console.log(matchArr);
+             return matchArr
+
+            }));
+            
+        };
+        fetchMatch()
+    }, [loggedMatches]);
+
+
+    console.log(matchArr);
+
+    // const matchArr = loggedUser.matches;
     return (
 
         <div>
@@ -43,21 +68,21 @@ function Profile() {
                                     <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
                                         <div className="relative">
                                             {/* profile picture */}
-                                            {/* <img
-                        alt="..."
-                        src={require(user.image).default}
-                        className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
-                        style={{ maxWidth: "150px" }}
-                      /> */}
+                                            <img
+                                                alt="..."
+                                                src={loggedImage}
+                                                className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
+                                                style={{ maxWidth: "150px" }}
+                                            />
                                         </div>
                                     </div>
                                 </div>
                                 <div className="text-center mt-12">
                                     <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
 
-                                        Welcome, {loggedUser.name}
-                  </h3>
+                                        Welcome, {loggedName}
 
+                                    </h3>
 
                                     <button
                                         className="bg-gradient-to-r from-pink to-purple uppercase text-white font-bold hover:sh adow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
@@ -80,6 +105,11 @@ function Profile() {
                                     <div className="col-span-1 text-center p-5 pt-20 bg-gradient-to-l from-red to-pink rounded-2xl">
                                         <h1 className="text-2xl">My Matches</h1>
                                         <p>It's time to find your playlist partner! </p>
+                                        <ul>
+                                            {loggedMatches.map(match => (
+                                                <li>{match}</li>
+                                            ))}
+                                        </ul>
 
                                     </div>
 
