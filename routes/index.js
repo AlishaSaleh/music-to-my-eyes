e719fdf681fde5d87be193b530aae3d6956816a6
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-06-12 00:32:02 +0100

    Route for build readded

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,8 @@ router.use("/user", userRoutes);
 router.use("/spotify", spotifyRoutes);
 
 // builds the react app if there are no API routes to hit
-// router.use((req, res) =>
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"))
-// );
+router.use((req, res) =>
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+);
 
 module.exports = router;
