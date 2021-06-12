24c0a90cb5f5412b078d472e4ec62f819d5aae68
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-06-12 02:29:48 +0100

    card styling

--- client/src/pages/Match.js
+++ client/src/pages/Match.js
@@ -23,7 +23,7 @@ function Match() {
     return (
         <div>
             <div className="w-full">
-                <div className="relative h-screen bg-red-light mx-auto flex flex-col items-center pt-12 sm:pt-24 pb-24">
+                <div className="relative h-full bg-red-light mx-auto flex flex-col items-center pt-12 sm:pt-24 pb-24">
                     <Card />
                 </div>
             </div>
